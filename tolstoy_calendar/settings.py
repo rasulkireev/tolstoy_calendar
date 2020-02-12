be6d05f2c3aa8a100c6729880386f56e1ae32cce
Rasul Kireev <kireevr1996@gmail.com>
2020-02-12 07:30:56 -0500

    Reqs updated

--- tolstoy_calendar/settings.py
+++ tolstoy_calendar/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env()
 # False if not in os.environ
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
