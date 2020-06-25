24a7f941d5cb5426fbfa65270b2e52b19508f898
Rasul Kireev <me@rasulkireev.com>
2020-06-24 21:55:50 -0400

    Scheduled every morning now.

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -3,7 +3,11 @@ from celery.decorators import periodic_task
 
 from newsletter.views import send_newsletter
 
-@periodic_task(run_every=(crontab(minute='*/1')), name="send_newsletter", ignore_result=True)
+@periodic_task(
+    run_every=(crontab(minute=0, hour=8)), 
+    name="send_newsletter", 
+    ignore_result=True
+)
 def run():
     send_newsletter()
     
